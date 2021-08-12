969f1e16038081e9b53d77b55a8eebfcca141293
vinolivae <vinicius.anjos@stone.com.br>
2021-08-12 01:56:54 -0300

    chore: add excoveralls

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule FlightBooking.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -24,7 +31,8 @@ defmodule FlightBooking.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_machina, "~> 2.7.0"},
-      {:elixir_uuid, "~> 1.2"}
+      {:elixir_uuid, "~> 1.2"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
